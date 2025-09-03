0305f205ec3900aecf1b9587807e341076565c8b
NU (Booskit) <Redbully14urh@gmail.com>
2025-09-03 16:50:43 +0000

    basic drizzle setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import * as schema from './schema';
+import 'dotenv/config';
 
-const sqlite = new Database('local.db');
+const sqlite = new Database(process.env.DB_FILE_NAME!);
 export const db = drizzle(sqlite, { schema });
