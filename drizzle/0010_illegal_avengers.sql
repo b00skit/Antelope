0f7c9c3f2746950efe6a777bd020fa284a83f81b
NU (Booskit) <Redbully14urh@gmail.com>
2025-09-13 09:27:58 +0000

    ensure that when you login via ouath if a user doesn't exist, you create

---
+++ drizzle/0010_illegal_avengers.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `factions` ADD `supervisor_rank` integer DEFAULT 10;--> statement-breakpoint
+ALTER TABLE `factions` ADD `minimum_abas` integer DEFAULT 0;--> statement-breakpoint
+ALTER TABLE `factions` ADD `minimum_supervisor_abas` integer DEFAULT 0;
